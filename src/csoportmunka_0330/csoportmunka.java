ab9596f7f496a9c664fede8894c450f55197d4fd
Ckody <barna.szantai2@gmail.com>
2021-04-04 16:42:18 +0200

    random szamok

--- src/csoportmunka_0330/csoportmunka.java
+++ src/csoportmunka_0330/csoportmunka.java
@@ -75,6 +75,11 @@ public class csoportmunka extends javax.swing.JFrame {
         jMenu1.setText("Feladatok");
 
         jMenuItem1.setText("Összeadás");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
         jMenu1.add(jMenuItem1);
 
         jMenuItem2.setText("Kivonás");
@@ -180,6 +185,10 @@ public class csoportmunka extends javax.swing.JFrame {
     private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jMenuItem4ActionPerformed
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
