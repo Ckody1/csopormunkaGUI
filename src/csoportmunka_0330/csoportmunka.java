fe2e0403786d2e33ad1af4f554b19907c2aecfd5
T. Virág <virmoc411@gmail.com>
2021-04-08 17:01:13 +0200

    Próba2

--- src/csoportmunka_0330/csoportmunka.java
+++ src/csoportmunka_0330/csoportmunka.java
@@ -221,6 +221,9 @@ public class csoportmunka extends javax.swing.JFrame {
     private void osztas(){
         
     }
+    private void kivonas(){
+        
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
